2cc79b3100f7c9f9648bdcb6038a347ee9e0d349
Nesibeli Gairatkyzy <nessibeli.gairatkyzy@narxoz.kz>
2026-03-03 22:49:04 +0500

    Implement Main demo with battle

--- src/com/narxoz/rpg/Main.java
+++ src/com/narxoz/rpg/Main.java
@@ -16,33 +16,38 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== RPG Battle Engine Demo ===\n");
 
-        // TODO: Create heroes and enemies
         Warrior warrior = new Warrior("Arthas");
         Mage mage = new Mage("Jaina");
-        Goblin goblin = new Goblin();
 
-        // TODO: Wrap with adapters
+
+        Goblin goblin1 = new Goblin();
+        Goblin goblin2 = new Goblin();
         List<Combatant> heroes = new ArrayList<>();
         heroes.add(new HeroCombatantAdapter(warrior));
         heroes.add(new HeroCombatantAdapter(mage));
 
         List<Combatant> enemies = new ArrayList<>();
-        enemies.add(new EnemyCombatantAdapter(goblin));
+        enemies.add(new EnemyCombatantAdapter(goblin1));
+        enemies.add(new EnemyCombatantAdapter(goblin2));
 
-        // TODO: Demonstrate Singleton behavior
+
+
         BattleEngine engineA = BattleEngine.getInstance();
         BattleEngine engineB = BattleEngine.getInstance();
-        System.out.println("Same instance? " + (engineA == engineB));
+        System.out.println("Singleton check: engineA == engineB ? " + (engineA == engineB));
         System.out.println();
 
-        // TODO: Run battle and print summary
+
         engineA.setRandomSeed(42L);
         EncounterResult result = engineA.runEncounter(heroes, enemies);
 
+
+        System.out.println("=== Battle Summary ===");
         System.out.println("Winner: " + result.getWinner());
-        System.out.println("Rounds: " + result.getRounds());
+        System.out.println("Rounds fought: " + result.getRounds());
+        System.out.println("\nBattle Log:");
         for (String line : result.getBattleLog()) {
-            System.out.println(line);
+            System.out.println("  " + line);
         }
 
         System.out.println("\n=== Demo Complete ===");
